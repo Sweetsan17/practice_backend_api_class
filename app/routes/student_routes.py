5549a0d73773e0f61b4a74107853d680e3da3a82
Sweetsan <coderintheworldhack100@gmail.com>
2026-05-31 18:46:54 +0530

    student routes file created

---
+++ app/routes/student_routes.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+from app.controllers.student_controller import *
+
+student_bp = Blueprint("students", __name__, url_prefix="/api/students")
+
+student_bp.route("", methods=["POST"])(create_student)

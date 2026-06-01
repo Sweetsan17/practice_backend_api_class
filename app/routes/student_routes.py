75aaa943e30b67a2c9ec4e259eea2ba7cc33b2cb
Sweetsan <coderintheworldhack100@gmail.com>
2026-06-01 09:48:53 +0530

    get all method routes created for student route

--- app/routes/student_routes.py
+++ app/routes/student_routes.py
@@ -4,3 +4,4 @@ from app.controllers.student_controller import *
 student_bp = Blueprint("students", __name__, url_prefix="/api/students")
 
 student_bp.route("", methods=["POST"])(create_student)
+student_bp.route("", methods=["GET"])(get_students)
